73b33b4ca40ca8a96474ccbb5def8bc0f0588e31
Nathanial O'Dell <nathanialodell01@gmail.com>
2025-12-29 17:04:51 -0500

    See prior.

--- R/zzz.R
+++ R/zzz.R
@@ -140,7 +140,7 @@ geocode_missing_coords <- function(df, state_name) {
     )
 
   df <- df %>%
-    dplyr::mutate(address_clean = stringr::str_to_upper(str_trim(address))) %>%
+    dplyr::mutate(address_clean = stringr::str_to_upper(stringr::str_trim(address))) %>%
     dplyr::left_join(
       geo.df %>%
         dplyr::mutate(address_clean = stringr::str_to_upper(stringr::str_trim(address))) %>%
