9278d5759c37d98d1f7cfad9cb53b2d2a3883769
nathanialaodell <175053516+nathanialaodell@users.noreply.github.com>
2025-12-31 13:47:25 -0800

    Update prism8_daily
    Added list of (potentially out of order) dates functionality.

--- R/prism8_daily.R
+++ R/prism8_daily.R
@@ -3,18 +3,40 @@
 #' @param var Character. The type of data you would like to download.  Must be "ppt", "tmean", "tmin", "tmax", "tdmean", "vpdmin", or "vpdmax".
 #' @param start_date Date. The first date in a range of days to download data for.
 #' @param end_date Date. The last date in a range of days to download data for.
-#' @param path Character. The location that data will be downloaded to. Defaults to working directory.
+#' @param dir Character. The location that data will be downloaded to. Defaults to working directory.
 #' @param remove Logical. Removes zip files after extraction; HIGHLY recommended. Defaults to TRUE.
+#' @param date_list List of date. If you already know which dates you'd like to download data frame, use this to avoid downloading irrelevant data. Prefered method.
 #' @import lubridate
 #' @import janitor
 #' @export
 
-prism8_daily <- function(var, start_date, end_date, path = getwd(), remove = TRUE){
+prism8_daily <- function(var, start_date, end_date, date_list = NULL,
+                         dir = getwd(), remove = TRUE){
 
-  dir <- path
   clim_var <- var
   base_url <- "https://services.nacse.org/prism/data/get/us/800m"
 
+  if (!is.null(date_list)){
+    for (i in seq_along(date_list)){
+      day <- strftime(dates_list[[i]], "%Y%m%d")
+      url <- paste0(base_url, "/", clim_var, "/", day, "?format=bil")
+
+      # Download file into temp folder
+      dest_file <- file.path(dir, paste0(clim_var, "_", day, ".bil.zip"))
+      download.file(url, destfile = dest_file, mode = "wb")
+
+      unzip(dest_file, exdir = dir)
+
+      if(remove){
+        file.remove(dest_file)
+      }
+
+      Sys.sleep(2)  # polite pause
+    }
+
+  }
+
+  else{
   dates <- seq(as.Date(start_date),
                as.Date(end_date),
                by = "1 day")
@@ -34,6 +56,7 @@ prism8_daily <- function(var, start_date, end_date, path = getwd(), remove = TRU
     }
 
     Sys.sleep(2)  # polite pause
+  }
   }
 
 }
