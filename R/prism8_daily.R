2724051ca546157de0c594a2f9c419a144e951d0
Nathanial O'Dell <nathanialodell01@gmail.com>
2026-01-01 14:46:45 -0800

    prism8_daily converts rasters to .tif

--- R/prism8_daily.R
+++ R/prism8_daily.R
@@ -1,4 +1,4 @@
-#' Extract daily PRISM data at 800m resolution.
+#' Download and convert daily PRISM data at 800m resolution to .tif files.
 #'
 #' @param var Character, or list of character. The type of data you would like to download.  Must be "ppt", "tmean", "tmin", "tmax", "tdmean", "vpdmin", or "vpdmax".
 #' @param start_date Date. The first date in a range of days to download data for.
@@ -6,15 +6,47 @@
 #' @param dir Character. The location that data will be downloaded to. Defaults to working directory.
 #' @param remove Logical. Removes zip files after extraction; HIGHLY recommended. Defaults to TRUE.
 #' @param date_list List of date. If you already know which dates you'd like to download data frame, use this to avoid downloading irrelevant data. Preferred method.
-#' @param bil Logical. Removes all extracted files that do not end in .bil; which is the minimum format needed for raster stacking. Defaults to TRUE.
+#' @param bil Logical. Removes all extracted files that do not end in .bil or .hdr; which are the minimum files needed to build a raster. Defaults to TRUE.
+#' @param template bbox. The extent(s) to which to crop downloaded data. See details
+#' @param state_name Character. If template is not missing, then the state(s) for which the bounding box is pulled from. Used for writing raster images.
+#' @examples
+#'
+#' box1 <- matrix(
+#'  c(-124.56624, 41.99179, -116.46350, 46.29083),
+#'  nrow = 1,
+#'  byrow = TRUE,
+#'  dimnames = list(NULL, c("xmin", "ymin", "xmax", "ymax"))
+#' )
+#'
+#' box2 <- matrix(
+#'  c(-106.64565, 25.83738, -93.50829, 36.50070),
+#'  nrow = 1,
+#'  byrow = TRUE,
+#'  dimnames = list(NULL, c("xmin", "ymin", "xmax", "ymax"))
+#' )
+#'
+#' boxes <- list(box1, box2)
+#'
+#' prism8_daily(var = c("ppt", "tmean", "tmin", "tmax"),
+#' start_date = "2025-12-25",
+#' end_date = "2025-12-26",
+#' template = boxes,
+#' state_name = c("OR", "TX"))
+#'
+#' @details
+#' If you intend to extract climate data for multiple, but not all, states, specify the template arg as a list of bounding boxes.
+#'
 #' @import lubridate
 #' @import janitor
+#' @import terra
 #' @export
 
 prism8_daily <- function(var, start_date, end_date, date_list = NULL,
-                         dir = getwd(), remove = TRUE, bil = TRUE){
+                         dir = getwd(), remove = TRUE, bil = TRUE,
+                         template, state_name = NULL){
 
   base_url <- "https://services.nacse.org/prism/data/get/us/800m"
+  CRS = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
 
   for (v in var){
 
@@ -38,17 +70,44 @@ prism8_daily <- function(var, start_date, end_date, date_list = NULL,
 
     ex_fl <- unzip(dest_file, exdir = dir)
 
-    non_bil <- ex_fl[!grepl("\\.bil$", ex_fl)]
+    irrelevant <- ex_fl[!grepl("\\.bil$|\\.hdr$", ex_fl)]
 
-    file.remove(non_bil)
+    relevant <- ex_fl[grepl("\\.bil$|\\.hdr$", ex_fl)]
+
+    bil <- ex_fl[grepl("\\.bil$", ex_fl)]
+
+    file.remove(irrelevant)
 
     if(remove){
       file.remove(dest_file)
     }
 
+    if (!missing(template)){
+
+      dat <- terra::rast(bil)
+      terra::project(dat, CRS)
+
+      for (k in seq_along(template)){
+
+        out <- terra::crop(
+          dat,
+          sf::st_transform(
+            template[[k]],
+            terra::crs(dat)
+          )
+        )
+
+        terra::writeRaster(out, paste0(state_name[k], "_", v, "_", day, ".tif"))
+
+        }
+
+      }
+
+      file.remove(relevant)
+    }
+
     Sys.sleep(2)  # polite pause
+
   }
 
     }
-
-  }
