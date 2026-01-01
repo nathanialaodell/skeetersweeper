43fc647311c02eb69440c3ac6361a329411dcaae
Nathanial O'Dell <nathanialodell01@gmail.com>
2026-01-01 08:13:07 -0800

    Merging.

---
+++ R/prism8_daily.R
@@ -0,0 +1,38 @@
+#' Extract daily PRISM data at 800m resolution.
+#'
+#' @param var Character. The type of data you would like to download.  Must be "ppt", "tmean", "tmin", "tmax", "tdmean", "vpdmin", or "vpdmax".
+#' @param start_date Date. The first date in a range of days to download data for.
+#' @param end_date Date. The last date in a range of days to download data for.
+#' @param path Character. The location that data will be downloaded to. Defaults to working directory.
+#' @import lubridate
+#' @import janitor
+#' @export
+
+prism8_daily <- function(var, start_date, end_date, path = getwd()){
+
+  dir <- path
+  clim_var <- var
+  base_url <- "https://services.nacse.org/prism/data/get/us/800m"
+
+  dates <- seq(as.Date(start_date),
+               as.Date(end_date),
+               by = "1 day")
+
+  for (i in seq_along(dates)) {
+    day <- strftime(dates[i], "%Y%m%d")
+    url <- paste0(base_url, "/", clim_var, "/", day, "?format=bil")
+
+    # Download file into temp folder
+    dest_file <- file.path(dir, paste0(clim_var, "_", day, ".bil.zip"))
+    download.file(url, destfile = dest_file, mode = "wb")
+
+    Sys.sleep(2)  # polite pause
+  }
+
+  zip_files <- list.files(dir, pattern = "\\.zip$", full.names = TRUE)
+
+  for (z in zip_files) {
+    unzip(z, exdir = dir)
+  }
+
+}
