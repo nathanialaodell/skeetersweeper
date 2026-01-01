2bf67865466f16550a82a205016b244b09cd836f
Nathanial O'Dell <nathanialodell01@gmail.com>
2026-01-01 08:42:37 -0800

    Added list of variables to download functionality

--- R/prism8_daily.R
+++ R/prism8_daily.R
@@ -1,6 +1,6 @@
 #' Extract daily PRISM data at 800m resolution.
 #'
-#' @param var Character. The type of data you would like to download.  Must be "ppt", "tmean", "tmin", "tmax", "tdmean", "vpdmin", or "vpdmax".
+#' @param var Character, or list of character. The type of data you would like to download.  Must be "ppt", "tmean", "tmin", "tmax", "tdmean", "vpdmin", or "vpdmax".
 #' @param start_date Date. The first date in a range of days to download data for.
 #' @param end_date Date. The last date in a range of days to download data for.
 #' @param dir Character. The location that data will be downloaded to. Defaults to working directory.
@@ -13,39 +13,26 @@
 prism8_daily <- function(var, start_date, end_date, date_list = NULL,
                          dir = getwd(), remove = TRUE){
 
-  clim_var <- var
   base_url <- "https://services.nacse.org/prism/data/get/us/800m"
 
+  for (v in var){
+
+
   if (!is.null(date_list)){
-    for (i in seq_along(date_list)){
-      day <- strftime(date_list[[i]], "%Y%m%d")
-      url <- paste0(base_url, "/", clim_var, "/", day, "?format=bil")
-
-      # Download file into temp folder
-      dest_file <- file.path(dir, paste0(clim_var, "_", day, ".bil.zip"))
-      download.file(url, destfile = dest_file, mode = "wb")
-
-      unzip(dest_file, exdir = dir)
-
-      if(remove){
-        file.remove(dest_file)
-      }
-
-      Sys.sleep(2)  # polite pause
-    }
-
+    dates <- date_list
   }
 
   else{dates <- seq(as.Date(start_date),
                as.Date(end_date),
                by = "1 day")
+  }
 
   for (i in seq_along(dates)) {
     day <- strftime(dates[i], "%Y%m%d")
-    url <- paste0(base_url, "/", clim_var, "/", day, "?format=bil")
+    url <- paste0(base_url, "/", v, "/", day, "?format=bil")
 
     # Download file into temp folder
-    dest_file <- file.path(dir, paste0(clim_var, "_", day, ".bil.zip"))
+    dest_file <- file.path(dir, paste0(v, "_", day, ".bil.zip"))
     download.file(url, destfile = dest_file, mode = "wb")
 
     Sys.sleep(2)  # polite pause
